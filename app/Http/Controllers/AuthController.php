5a69ce4ba48ec44e5991e1c679e47e4d169f7d45
yigit <yigittanriverdi@yandex.com>
2017-08-20 18:29:42 +0300

    Route Modules Done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function store(Request $request){
+
+    }
+
+    public function signin(Request $request){
+
+    }
+}
