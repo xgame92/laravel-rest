49805be4b20dfdd2c9229a34089303fc02be5b61
yigit <yigittanriverdi@yandex.com>
2017-08-20 19:12:58 +0300

    Meeting Controller edited

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class MeetingController extends Controller
 {
+    public function __construct()
+    {
+        // $this->middleware('name');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -13,18 +18,42 @@ class MeetingController extends Controller
      */
     public function index()
     {
-        return "it works!";
-    }
+        $meeting = [
+            'title' => 'Title',
+            'description' => 'Description',
+            'time' => 'Time',
+            'user_id' => 'User Id',
+            'view_meeting' => [
+                'href' => 'api/v1/meeting/1',
+                'method' => 'GET'
+            ]
+        ];
 
+        $response = [
+            'msg' => 'List of all Meetings',
+            'meetings' => [
+                $meeting,
+                $meeting
+            ]
+        ];
+        return response()->json($response, 200);
+    }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required',
+            'time' => 'required|date_format:YmdHie',
+            'user_id' => 'required'
+        ]);
+
         $title = $request->input('title');
         $description = $request->input('description');
         $time = $request->input('time');
@@ -41,54 +70,96 @@ class MeetingController extends Controller
             ]
         ];
 
-        $response =[
+        $response = [
             'msg' => 'Meeting created',
             'meeting' => $meeting
         ];
 
-        return response()->json($response,201);
-
+        return response()->json($response, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        return "it works!";
+        $meeting = [
+            'title' => 'Title',
+            'description' => 'Description',
+            'time' => 'Time',
+            'user_id' => 'User Id',
+            'view_meetings' => [
+                'href' => 'api/v1/meeting',
+                'method' => 'GET'
+            ]
+        ];
 
+        $response = [
+            'msg' => 'Meeting information',
+            'meeting' => $meeting
+        ];
+        return response()->json($response, 200);
     }
-
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required',
+            'time' => 'required|date_format:YmdHie',
+            'user_id' => 'required'
+        ]);
+
         $title = $request->input('title');
         $description = $request->input('description');
         $time = $request->input('time');
         $user_id = $request->input('user_id');
-        return "it works!";
+        $meeting = [
+            'title' => $title,
+            'description' => $description,
+            'time' => $time,
+            'user_id' => $user_id,
+            'view_meeting' => [
+                'href' => 'api/v1/meeting/1',
+                'method' => 'GET'
+            ]
+        ];
 
+        $response = [
+            'msg' => 'Meeting updated',
+            'meeting' => $meeting
+        ];
+
+        return response()->json($response, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        return "it works!";
+        $response = [
+            'msg' => 'Meeting deleted',
+            'create' => [
+                'href' => 'api/v1/meeting',
+                'method' => 'POST',
+                'params' => 'title, description, time'
+            ]
+        ];
 
+        return response()->json($response, 200);
     }
 }
