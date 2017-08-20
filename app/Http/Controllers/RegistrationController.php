d091f5691565e7ba50f6c2b5a8dd7fdef18afb3e
yigit <yigittanriverdi@yandex.com>
2017-08-20 19:13:44 +0300

    Registration Controller edited

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -6,8 +6,6 @@ use Illuminate\Http\Request;
 
 class RegistrationController extends Controller
 {
-
-
     /**
      * Store a newly created resource in storage.
      *
@@ -16,10 +14,39 @@ class RegistrationController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'meeting_id' => 'required',
+            'user_id' => 'required',
+        ]);
+
         $meeting_id = $request->input('meeting_id');
         $user_id = $request->input('user_id');
-        return "it works!";
 
+        $meeting = [
+            'title' => 'Title',
+            'description' => 'Description',
+            'time' => 'Time',
+            'view_meeting' => [
+                'href' => 'api/v1/meeting/1',
+                'method' => 'GET'
+            ]
+        ];
+
+        $user = [
+            'name' => 'Name'
+        ];
+
+        $response = [
+            'msg' => 'User registered for meeting',
+            'meeting' => $meeting,
+            'user' => $user,
+            'unregister' => [
+                'href' => 'api/v1/meeting/registration/1',
+                'method' => 'DELETE'
+            ]
+        ];
+
+        return response()->json($response, 201);
     }
 
     /**
@@ -30,7 +57,31 @@ class RegistrationController extends Controller
      */
     public function destroy($id)
     {
-        return "it works!";
+        $meeting = [
+            'title' => 'Title',
+            'description' => 'Description',
+            'time' => 'Time',
+            'view_meeting' => [
+                'href' => 'api/v1/meeting/1',
+                'method' => 'GET'
+            ]
+        ];
 
+        $user = [
+            'name' => 'Name'
+        ];
+
+        $response = [
+            'msg' => 'User unregistered for meeting',
+            'meeting' => $meeting,
+            'user' => $user,
+            'register' => [
+                'href' => 'api/v1/meeting/registration',
+                'method' => 'POST',
+                'params' => 'user_id, meeting_id'
+            ]
+        ];
+
+        return response()->json($response, 200);
     }
 }
